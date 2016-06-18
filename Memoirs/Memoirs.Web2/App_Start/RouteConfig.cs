04f374b946546ab64c483e1a81a96d70212ef886
alexey-aristov <alexey0206@gmail.com>
2016-06-18 14:07:50 +0400

    issue with fonts fixed

--- Memoirs/Memoirs.Web2/App_Start/RouteConfig.cs
+++ Memoirs/Memoirs.Web2/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,12 +13,38 @@ namespace Memoirs.Web2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.Add(new Route("fonts/{file}", new FontsRouteHadler()));
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                );
         }
+
+
+    }
+    class FontsRouteHadler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            var fileName = requestContext.RouteData.GetRequiredString("file");
+            return new FontsHttpHandler(fileName);
+        }
+    }
+
+    class FontsHttpHandler : IHttpHandler
+    {
+        private string _file;
+        public FontsHttpHandler(string file)
+        {
+            _file = file;
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.WriteFile(Path.Combine(context.Request.PhysicalApplicationPath, "node_modules/bootstrap/dist/fonts", _file));
+        }
+
+        public bool IsReusable => true;
     }
 }
